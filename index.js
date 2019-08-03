70e833e021a42c0d9549d96bf507249c4d23c732
Nelson Chen <nelsonchen5@gmail.com>
2019-08-03 15:45:37 -0700

    changed overlay color

--- index.js
+++ index.js
@@ -1,20 +1,22 @@
-import React from 'react';
-import PropTypes from 'prop-types';
-import { Modal, TouchableWithoutFeedback, View } from 'react-native';
-import * as Animatable from 'react-native-animatable';
+import React from "react";
+import PropTypes from "prop-types";
+import { Modal, TouchableWithoutFeedback, View } from "react-native";
+import * as Animatable from "react-native-animatable";
 
-const AnimatableTouchableWithoutFeedback = Animatable.createAnimatableComponent(TouchableWithoutFeedback);
+const AnimatableTouchableWithoutFeedback = Animatable.createAnimatableComponent(
+  TouchableWithoutFeedback
+);
 const styles = {
   container: {
     flex: 1,
-    justifyContent: 'center',
+    justifyContent: "center",
     padding: 20,
-    backgroundColor: 'rgba(37, 8, 10, 0.78)',
+    backgroundColor: "rgba(0, 0, 0, 0.78)"
   },
   innerContainer: {
-    alignItems: 'center',
-    backgroundColor: 'white',
-    padding: 20,
+    alignItems: "center",
+    backgroundColor: "white",
+    padding: 20
   }
 };
 
@@ -22,7 +24,7 @@ class Overlay extends React.Component {
   state = {
     visible: this.props.visible,
     animationType: this.props.animationType,
-    overlayAnimationType: 'fadeIn'
+    overlayAnimationType: "fadeIn"
   };
   static propTypes = {
     children: PropTypes.oneOfType([PropTypes.node, PropTypes.func]).isRequired,
@@ -33,21 +35,24 @@ class Overlay extends React.Component {
     closeOnTouchOutside: PropTypes.bool,
     onClose: PropTypes.func.isRequired,
     containerStyle: PropTypes.oneOfType([PropTypes.object, PropTypes.number]),
-    childrenWrapperStyle: PropTypes.oneOfType([PropTypes.object, PropTypes.number]),
+    childrenWrapperStyle: PropTypes.oneOfType([
+      PropTypes.object,
+      PropTypes.number
+    ]),
     animationDuration: PropTypes.number,
-    accessible: PropTypes.bool,
-  }
+    accessible: PropTypes.bool
+  };
   static defaultProps = {
     children: null,
-    animationType: 'fadeIn',
-    animationOutType: 'fadeOut',
-    easing: 'ease',
+    animationType: "fadeIn",
+    animationOutType: "fadeOut",
+    easing: "ease",
     visible: false,
     closeOnTouchOutside: false,
-    onClose: () => { },
+    onClose: () => {},
     animationDuration: 500,
-    accessible: true,
-  }
+    accessible: true
+  };
 
   static getDerivedStateFromProps(props) {
     return { visible: props.visible, animationType: props.animationType };
@@ -55,35 +60,62 @@ class Overlay extends React.Component {
 
   _hideModal = () => {
     const { animationOutType, animationDuration, onClose } = this.props;
-    this.setState({ animationType: animationOutType, overlayAnimationType: animationOutType });
+    this.setState({
+      animationType: animationOutType,
+      overlayAnimationType: animationOutType
+    });
     let timer = setTimeout(() => {
       onClose();
       clearTimeout(timer);
-      this.setState({ overlayAnimationType: 'fadeIn' });
+      this.setState({ overlayAnimationType: "fadeIn" });
     }, animationDuration - 100);
-  }
+  };
 
-  _stopPropagation = (e) => e.stopPropagation()
-
+  _stopPropagation = e => e.stopPropagation();
 
   render() {
-    const { closeOnTouchOutside, animationDuration, children,
-          accessible, 
-      containerStyle, childrenWrapperStyle, easing, ...extraProps } = this.props;
+    const {
+      closeOnTouchOutside,
+      animationDuration,
+      children,
+      accessible,
+      containerStyle,
+      childrenWrapperStyle,
+      easing,
+      ...extraProps
+    } = this.props;
     return (
       <Modal
         transparent
         visible={this.state.visible}
         onRequestClose={this._hideModal}
         onDismiss={this._hideModal}
-        {...extraProps} animationType='none'>
-        <TouchableWithoutFeedback onPress={closeOnTouchOutside ? this._hideModal : null} accessible={accessible}>
-          <Animatable.View animation={this.state.overlayAnimationType} duration={animationDuration} easing={easing}
-            useNativeDriver style={[styles.container, containerStyle]}>
-            <AnimatableTouchableWithoutFeedback accessible={accessible} animation={this.state.animationType} easing={easing}
-              duration={animationDuration} useNativeDriver onPress={this._stopPropagation}>
+        {...extraProps}
+        animationType="none"
+      >
+        <TouchableWithoutFeedback
+          onPress={closeOnTouchOutside ? this._hideModal : null}
+          accessible={accessible}
+        >
+          <Animatable.View
+            animation={this.state.overlayAnimationType}
+            duration={animationDuration}
+            easing={easing}
+            useNativeDriver
+            style={[styles.container, containerStyle]}
+          >
+            <AnimatableTouchableWithoutFeedback
+              accessible={accessible}
+              animation={this.state.animationType}
+              easing={easing}
+              duration={animationDuration}
+              useNativeDriver
+              onPress={this._stopPropagation}
+            >
               <View style={[styles.innerContainer, childrenWrapperStyle]}>
-                {children instanceof Function ? children(this._hideModal, this.state) : children}
+                {children instanceof Function
+                  ? children(this._hideModal, this.state)
+                  : children}
                 {/* Passing this reference in render prop so that the parent component can access all the children's methods */}
               </View>
             </AnimatableTouchableWithoutFeedback>
